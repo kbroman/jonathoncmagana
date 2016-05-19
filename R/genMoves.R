7f6f10282f8709bbc040b73a72c5424a05854d00
Karl Broman <kbroman@gmail.com>
2016-05-19 05:53:53 -0400

    remove semi-colons from R code (not needed in R)

--- R/genMoves.R
+++ R/genMoves.R
@@ -14,28 +14,28 @@ genMoves <-
     function(N = 1000, sigma = .1)
 {
     # uncomment to set values for testing
-    # N = 1000;
-      # sigma = .1;
+    # N = 1000
+      # sigma = .1
 
     # generate N values with mean of 0 and standard deviation of 1
-    moves_x = rnorm(N, 0, 1);
-    moves_y = rnorm(N, 0, 1);
+    moves_x = rnorm(N, 0, 1)
+    moves_y = rnorm(N, 0, 1)
 
     # generate N values with mean of 0 and standard deviation of sigma^2
-    epsilon_x = rnorm(N, 0, sigma*sigma);
-    epsilon_y = rnorm(N, 0, sigma*sigma);
+    epsilon_x = rnorm(N, 0, sigma*sigma)
+    epsilon_y = rnorm(N, 0, sigma*sigma)
 
     # added epsilon value to the first entry
-    moves_x[1] = moves_x[1] + epsilon_x[1];
-    moves_y[1] = moves_y[1] + epsilon_y[1];
+    moves_x[1] = moves_x[1] + epsilon_x[1]
+    moves_y[1] = moves_y[1] + epsilon_y[1]
 
 
     # update moves vector to be moves[i-1]*epsilon[i]
     for (i in 2:N) {
-            moves_x[i]=moves_x[i-1]+epsilon_x[i];
-        moves_y[i]=moves_y[i-1]+epsilon_y[i];
+            moves_x[i]=moves_x[i-1]+epsilon_x[i]
+        moves_y[i]=moves_y[i-1]+epsilon_y[i]
     }
 
     # combine two vectors into array for output
-    rbind(moves_x, moves_y);
+    rbind(moves_x, moves_y)
 }
--- R/plotMoves.R
+++ R/plotMoves.R
@@ -14,5 +14,5 @@ plotMoves <-
     function(moves)
 {
     # plot the vector, this will eventually be done in a second function
-    plot(moves[1,], moves[2,], type="l",  main= "Brownian Motion in Two Dimensions", xlab="X", ylab="Y");
+    plot(moves[1,], moves[2,], type="l",  main= "Brownian Motion in Two Dimensions", xlab="X", ylab="Y")
 }
